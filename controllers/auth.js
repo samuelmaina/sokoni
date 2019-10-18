6c30d43de8cf757d4e6df50a44c80ba40e7f6f33
samuelmayna <samuelmayna@gmail.com>
2019-10-18 23:54:17 +0300

    modify user to be created at post login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,14 +9,14 @@ exports.getLogin = (req, res, next) => {
 };
 exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
-  // User.findById("5da1f8947fb2132c1c7d450d")
-  // .then(user => {
-  //   console.log('Entering the found user in the session');
-  //   req.session.user=user;
+  User.findById("5da1f8947fb2132c1c7d450d")
+  .then(user => {
+    console.log('Entering the found user in the session');
+    req.session.user=user;
     
-  //   console.log(user);
-  // })
-  // .catch(err => console.log(err));
+    console.log(user);
+  })
+  .catch(err => console.log(err));
   res.redirect("/");
 };
 
