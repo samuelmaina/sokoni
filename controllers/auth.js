a07e2fff7d8a6c9064f5bc9a7ce5a88d9e96e4c5
samuelmayna <samuelmayna@gmail.com>
2019-10-20 18:45:45 +0300

    Add new two routes for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,38 @@
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.isLoggedIn);
+  console.log(req.session);
   res.render("auth/login", {
     pageTitle: "login",
     path: "/auth/login"
   });
 };
+
+
+exports.getSignUp= (req, res, next) => {
+  res.render("auth/signup", {
+    pageTitle: "Sign UP ",
+    path: "/auth/signup"
+  });
+};
+
+
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
   User.findById("5da1f8947fb2132c1c7d450d")
-  .then(user => {
-    console.log('Entering the found user in the session');
-    req.session.user=user;
-    
-    console.log(user);
-  })
-  .catch(err => console.log(err));
-  res.redirect("/");
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect("/");
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-
   req.session.destroy(err => {
     if (err) {
       console.log(err);
