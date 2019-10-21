cb8475cff173a55cd27deb655c8348fc2425c0c8
samuelmayna <samuelmayna@gmail.com>
2019-10-21 09:42:51 +0300

    add admin schema to to store admins' data

---
+++ models/admin.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const AdminSchema = new Schema({
+  adminName: {
+    type: String,
+    required: true
+  },
+  adminEmail: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+module.exports=mongoose.model('Admin',AdminSchema)
