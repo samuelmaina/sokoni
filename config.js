bb90658b3769e2783801b43c7f90ae1b3eaaa4d9
samuelmayna <samuelmayna@gmail.com>
2020-10-29 09:33:53 +0300

    added paths to the config

--- config.js
+++ config.js
@@ -1,2 +1,10 @@
 require("dotenv").config();
-module.exports = process.env;
+const envVaribles = process.env;
+
+module.exports = {
+  PORT: envVaribles.PORT,
+  MONGO_URI: envVaribles.MONGO_URI,
+  PRODUCTS_PER_PAGE: Number(envVaribles.PRODUCTS_PER_PAGE),
+  SESSION_STORE: envVaribles.SESSION_STORE,
+  SESSION_SECRET: envVaribles.SESSION_SECRET,
+};
