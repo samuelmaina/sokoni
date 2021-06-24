7af24dccc71b5d3feab3055544ae5fa81a225755
samuelmayna <samuelmayna@gmail.com>
2021-06-24 12:53:47 +0300

    added tests for adding and modifying products

--- tests/e2e/admin.test.js
+++ tests/e2e/admin.test.js
@@ -1,7 +1,10 @@
 const { startApp, closeApp, getNewDriverInstance } = require('./config');
 const path = require('path');
 
-const { generateStringSizeN } = require('../utils/generalUtils/utils');
+const {
+	generateStringSizeN,
+	generateMongooseId,
+} = require('../utils/generalUtils/utils');
 const { verifyEqual, verifyTruthy } = require('../utils/testsUtils');
 
 const { clearDb, clearModel } = require('../utils/generalUtils').database;
@@ -19,6 +22,12 @@ const {
 } = require('./utils/generalUtils');
 const { product } = require('../../config/constraints');
 const { Product } = require('../../database/models');
+const {
+	createTestProducts,
+	createDocForType,
+} = require('../utils/generalUtils/database');
+const { By } = require('selenium-webdriver');
+const { ensureObjectsHaveSameFields } = require('../models/utils');
 const { ensureHasTitle } = generalUtils;
 
 const MAX_SETUP_TIME = 25000;
@@ -34,11 +43,34 @@ const data = {
 };
 const logInUrl = `${base}/auth/admin/log-in`;
 let page;
+
+const validProduct = {
+	title: 'test 1',
+	file: path.resolve('tests/data/images/199707738.jpg'),
+	buyingPrice: 69.99,
+	percentageProfit: 20,
+	quantity: 200,
+	brand: 'The good Brand',
+	category: 'clothing',
+	description: 'The product was very good I  loved it.',
+};
+
+const invalidProduct = {
+	title: 'te',
+	file: path.resolve('tests/data/images/199707738.jpg'),
+	buyingPrice: 69.99,
+	percentageProfit: 20,
+	quantity: 200,
+	brand: 'The good Brand',
+	category: 'clothing',
+	description: 'The product was very good I  loved it.',
+};
 describe('Auth', () => {
+	let admin;
 	beforeAll(async () => {
 		await startApp(PORT);
 		page = new Page(getNewDriverInstance());
-		await utilLogin(page, logInUrl, data, 'admin');
+		admin = await utilLogin(page, logInUrl, data, 'admin');
 		await ensureHasTitle(page, 'Your Products');
 	}, MAX_SETUP_TIME);
 	afterAll(async () => {
@@ -48,7 +80,7 @@ describe('Auth', () => {
 		await closeApp();
 	});
 
-	describe('Add Products', () => {
+	describe('Add Product', () => {
 		beforeEach(async () => {
 			const url = `${base}/admin/add-product`;
 			await page.openUrl(url);
@@ -59,58 +91,104 @@ describe('Auth', () => {
 		it(
 			'should upload a product ',
 			async () => {
-				const valid = {
-					title: 'test 1',
-					file: path.resolve('tests/data/images/199707738.jpg'),
-					bP: 69.99,
-					pProfit: 20,
-					quantity: 200,
-					brand: 'The good Brand',
-					category: 'clothing',
-					description: 'The product was very good I  loved it.',
-				};
-				await enterProductData(valid);
+				await enterProductData(validProduct);
 				await ensureHasTitleAndInfo(
 					page,
 					'Your Products',
 					'Product added successfully.'
 				);
+				await ensureProductExistUsingItsTitle(validProduct.title);
 			},
 			MAX_TESTING_TIME
 		);
 		it(
 			'should refuse if the product data is incorrect ',
 			async () => {
-				const invalid = {
-					title: 'te',
-					file: path.resolve('tests/data/images/199707738.jpg'),
-					bP: 69.99,
-					pProfit: 20,
-					quantity: 200,
-					brand: 'The good Brand',
-					category: 'clothing',
-					description: 'The product was very good I  loved it.',
-				};
-				await enterProductData(invalid);
+				await enterProductData(invalidProduct);
 				await ensureHasTitleAndError(
 					page,
 					'Add Product',
 					`${product.title.error}`
 				);
+				//errernous products should not be added to the database.
+				await ensureProductDoesNotExistUsingItsTitle(invalidProduct.title);
 			},
 			MAX_TESTING_TIME
 		);
 	});
+	describe('Edit Product', () => {
+		let created;
+		beforeEach(async () => {
+			created = (await createTestProducts([admin.id], 3))[0];
+			const url = `${base}/admin/products`;
+			await page.openUrl(url);
+			await clickOneEdit();
+		});
+		afterEach(async () => {
+			await clearModel(Product);
+		});
+		it(
+			'should update for correct data',
+			async () => {
+				await page.hold(400);
+				await enterProductData(validProduct);
+				await ensureHasTitleAndInfo(
+					page,
+					'Your Products',
+					'Product updated successfully.'
+				);
+
+				const update = await findProductByTitle(validProduct.title);
+				ensureObjectsHaveSameFields(update, validProduct, [
+					'title',
+					'description',
+				]);
+			},
+			MAX_TESTING_TIME
+		);
+
+		it(
+			'should  refuse for invalid data',
+			async () => {
+				await page.hold(400);
+				await enterProductData(invalidProduct);
+				await ensureHasTitleAndError(page, 'Edit Product', product.title.error);
+				const retrieved = await Product.findById(created.id);
+				//ensure product data is not updated
+				ensureObjectsHaveSameFields(retrieved, created, [
+					'title',
+					'description',
+					'buyingPrice',
+				]);
+			},
+			MAX_TESTING_TIME
+		);
+	});
+
 	async function enterProductData(product) {
 		const prodPage = new ProductPage(page);
 		await prodPage.enterTitle(product.title);
 		await prodPage.chooseFIle(product.file);
-		await prodPage.enterBuyingPrice(product.bP);
-		await prodPage.enterPercentageProfit(product.pProfit);
+		await prodPage.enterBuyingPrice(product.buyingPrice);
+		await prodPage.enterPercentageProfit(product.percentageProfit);
 		await prodPage.enterQuantity(product.quantity);
 		await prodPage.enterBrand(product.brand);
 		await prodPage.enterCategory(product.category);
 		await prodPage.enterDescription(product.description);
 		await prodPage.submit();
 	}
+	async function clickOneEdit() {
+		const articles = await page.getELements('article');
+		await articles[0].findElement(By.className(`edit_product`)).click();
+	}
 });
+
+async function ensureProductExistUsingItsTitle(title) {
+	expect(await findProductByTitle(title)).not.toBeNull();
+}
+async function ensureProductDoesNotExistUsingItsTitle(title) {
+	expect(await findProductByTitle(title)).toBeNull();
+}
+async function findProductByTitle(title) {
+	return await Product.findOne({ title });
+}
