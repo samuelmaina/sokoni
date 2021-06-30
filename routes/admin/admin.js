968614ce3ca9d39465e04742ee7bf4009a4fd529
samuelmayna <samuelmayna@gmail.com>
2021-06-30 11:40:04 +0300

    combined all the product validator into one array which are passed as a middleware

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 
-const {admin} = require("../../controllers");
+const { admin } = require('../../controllers');
 
-const {product} = require("../../validators");
-
+const { product } = require('../../validators');
+const productValidtor = product.productValidator;
 
 const controller = admin;
 
 const router = express.Router();
 
 router
-  .route("/add-product")
-  .get(controller.getAddProduct)
-  .post(product, controller.postAddProduct);
+	.route('/add-product')
+	.get(controller.getAddProduct)
+	.post(productValidtor, controller.postAddProduct);
 
-router.get("/edit-product/:id", controller.getEditProduct);
-router.post("/edit-product", product, controller.postEditProduct);
+router.get('/edit-product/:id', controller.getEditProduct);
+router.post('/edit-product', productValidtor, controller.postEditProduct);
 
-router.get("/products", controller.getProducts);
-router.delete("/product/:id", controller.deleteProduct);
-router.get("/get-admin-sales", controller.getAdminSales);
+router.get('/products', controller.getProducts);
+router.delete('/product/:id', controller.deleteProduct);
+router.get('/get-admin-sales', controller.getAdminSales);
 
 module.exports = router;
