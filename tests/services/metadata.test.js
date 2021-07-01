80afeff54298460456ee0c49b40ff6f1e46e8522
samuelmayna <samuelmayna@gmail.com>
2021-07-01 11:17:49 +0300

    modified the tests to accommodate for addition of admins

--- tests/services/metadata.test.js
+++ tests/services/metadata.test.js
@@ -1,18 +1,36 @@
 const { addElementIfNonExisting } = require('../../database/services/metadata');
 const { ensureArrayContains, verifyEqual } = require('../utils/testsUtils');
 
-describe('should be able to add element to arr', () => {
+describe.skip('should be able to add element to arr', () => {
 	it('when there are no preexisting element', () => {
-		const firstElement = 'element 1';
+		const firstElement = {
+			category: 'category 1',
+			adminId: 'qe98r9re0r9e9re',
+		};
 		const arr = [];
 		addElementIfNonExisting(arr, firstElement);
-		ensureArrayContains(arr, firstElement);
+		const stored = arr[0];
+		verifyEqual(firstElement.category, stored.category);
+		ensureArrayContains(stored.adminIds, firstElement.adminId);
 	});
-	it('should not add element if the elem already exists in the array', () => {
-		const firstElement = 'element 1';
-		const arr = [firstElement];
-		addElementIfNonExisting(arr, firstElement);
+	it('should not add element if the elem already exists in the array but only add the admin Id ', () => {
+		const testCategory = 'category 1';
+		const category1 = {
+			category: testCategory,
+			adminId: 'qe98r9re0r9e9re',
+		};
+		const category1DifferentAdmin = {
+			category: testCategory,
+			adminId: 'qljfkldjlfjdl',
+		};
+		const arr = [];
+		addElementIfNonExisting(arr, category1);
+		addElementIfNonExisting(arr, category1DifferentAdmin);
 		verifyEqual(arr.length, 1);
-		ensureArrayContains(arr, firstElement);
+		const stored = arr[0];
+		verifyEqual(stored.category, testCategory);
+		const adminIds = stored.adminIds;
+		ensureArrayContains(adminIds, category1.adminId);
+		ensureArrayContains(adminIds, category1DifferentAdmin.adminId);
 	});
 });
