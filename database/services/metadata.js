78db5c3b0725989622b9bd16afba8bb18278afca
samuelmayna <samuelmayna@gmail.com>
2021-07-01 12:07:58 +0300

    modified the function to work with any field. Earlier the field worked only with the category field

--- database/services/metadata.js
+++ database/services/metadata.js
@@ -1,13 +1,14 @@
-exports.addElementIfNonExisting = (arr, element) => {
-	const { category, adminId } = element;
+exports.addElementIfNonExisting = (field, arr, element) => {
+	const { adminId } = element;
+	const typeData = element[field];
 	const elementIndex = arr.findIndex(c => {
-		return c.category === element.category;
+		return c[field] === typeData;
 	});
 	if (elementIndex < 0) {
-		arr.push({
-			category: category,
-			adminIds: [adminId],
-		});
+		const object = {};
+		object[field] = typeData;
+		object.adminIds = [adminId];
+		arr.push(object);
 	} else {
 		const elem = arr[elementIndex];
 		elem.adminIds.push(adminId);
