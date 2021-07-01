6708e27e2f785e3c0cf1a06f90fa7990b2889303
samuelmayna <samuelmayna@gmail.com>
2021-07-01 11:17:02 +0300

    modified the the category metadata table to accept admin

--- database/services/metadata.js
+++ database/services/metadata.js
@@ -1,8 +1,15 @@
 exports.addElementIfNonExisting = (arr, element) => {
+	const { category, adminId } = element;
 	const elementIndex = arr.findIndex(c => {
-		return c === element;
+		return c.category === element.category;
 	});
 	if (elementIndex < 0) {
-		arr.push(element);
+		arr.push({
+			category: category,
+			adminIds: [adminId],
+		});
+	} else {
+		const elem = arr[elementIndex];
+		elem.adminIds.push(adminId);
 	}
 };
