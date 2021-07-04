5a9e69346d2e6343e7024c0721582d12371bdd9d
samuelmayna <samuelmayna@gmail.com>
2021-07-04 08:16:19 +0300

    Added tests to check if product is added to cart

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -5,7 +5,11 @@ const {
 } = require('../utils/generalUtils/database');
 const baseTest = require('./baseAdminAndUser');
 const { generateMongooseId } = require('../utils/generalUtils/utils');
-const { verifyEqual, ensureArrayContains } = require('../utils/testsUtils');
+const {
+	verifyEqual,
+	ensureArrayContains,
+	ensureObjectHasKeyValuePair,
+} = require('../utils/testsUtils');
 const { includeSetUpAndTearDown } = require('./utils');
 const credentials = {
 	name: 'John Doe',
@@ -15,7 +19,9 @@ const credentials = {
 
 describe('User test', () => {
 	includeSetUpAndTearDown();
-	baseTest(User);
+	describe.skip('Auth tests', () => {
+		baseTest(User);
+	});
 	describe('user methods', () => {
 		let user;
 		beforeEach(async () => {
@@ -25,13 +31,15 @@ describe('User test', () => {
 			await clearDb();
 		});
 		describe('user methods', () => {
-			it('should ', async () => {
+			it('should add product to cart ', async () => {
 				const productId = generateMongooseId();
 				const quantity = 50;
 				await user.addProductsToCart(productId, quantity);
 				const cart = user.cart;
 				verifyEqual(cart.length, 1);
-				ensureArrayContains(cart, { prodcutData: productId, quantity });
+				const first = cart[0];
+				ensureObjectHasKeyValuePair(first, 'productData', productId);
+				ensureObjectHasKeyValuePair(first, 'quantity', quantity);
 			});
 		});
 	});
