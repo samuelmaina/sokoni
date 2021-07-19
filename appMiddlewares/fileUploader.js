e4f503e1b886850a9b734a99e4dbf725168b2459
samuelmayna <samuelmayna@gmail.com>
2021-07-19 13:45:56 +0300

    changed the entry script

--- appMiddlewares/fileUploader.js
+++ appMiddlewares/fileUploader.js
@@ -1,36 +1,45 @@
-const multer = require("multer");
+const multer = require('multer');
 
-const fileDestination = "Data/Images";
-const fileFieldName = "image";
+const fileFieldName = 'image';
+
+function determineWhereToSave() {
+	let args = process.argv;
+	const productionDestination = 'images';
+	const productFed = args[2].split('=')[1];
+	if (productFed === productionDestination) return 'data/'.concat(productFed);
+	const testFed = args[4].split('=')[1];
+	return 'data/'.concat(testFed);
+}
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, determineWhereToSave());
+	},
+	filename: (req, file, cb) => {
+		// generate random numbers to make file names unique
+		cb(null, Math.random() + '-' + file.originalname);
+	},
+});
+
 const filter = (req, file, cb) => {
-  const fileType = file.mimetype;
-  if (isImage(fileType)) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+	const fileType = file.mimetype;
+	if (isImage(fileType)) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
 };
 const isImage = fileType => {
-  return (
-    fileType === "image/png" ||
-    fileType === "image/jpg" ||
-    fileType === "image/jpeg"
-  );
+	return (
+		fileType === 'image/png' ||
+		fileType === 'image/jpg' ||
+		fileType === 'image/jpeg'
+	);
 };
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, fileDestination);
-  },
-  filename: (req, file, cb) => {
-    // generate random numbers to make file names unique
-    cb(null, Math.random() + "-" + file.originalname);
-  },
-});
-const multerSettings = {storage: fileStorage, fileFilter: filter};
+const multerSettings = { storage: fileStorage, fileFilter: filter };
 /**
  * @returns - returns an image uploader for express app
  */
 const fileUploader = multer(multerSettings).single(fileFieldName);
 module.exports = app => {
-  app.use(fileUploader);
+	app.use(fileUploader);
 };
