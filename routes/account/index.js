ae6d717154af369ea46ab36d6867b57d161256c7
samuelmayna <samuelmayna@gmail.com>
2021-07-19 15:15:17 +0300

    modified

--- routes/account/index.js
+++ routes/account/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-	ensureUserIsAuth,
-	ensureAdminIsAuth,
-} = require('../../authMiddleware/index');
-
 const userRoutes = require('./user');
 const adminRoutes = require('./admin');
+
+const { ensureUserIsAuth, ensureAdminIsAuth } = require('../../authMiddleware');
 
 router.use('/user', ensureUserIsAuth, userRoutes);
 router.use('/admin', ensureAdminIsAuth, adminRoutes);
