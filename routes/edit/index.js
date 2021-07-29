ce7b70ac78376bf60f0e25fd94c513930c6b701a
samuelmayna <samuelmayna@gmail.com>
2021-07-29 12:35:38 +0300

    resolved the require error in heroku

--- routes/edit/index.js
+++ routes/edit/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const {ensureUserIsAuth, ensureAdminIsAuth} = require("../../authmiddleware");
+const { ensureUserIsAuth, ensureAdminIsAuth } = require('../../authMiddleware');
 
-const userEditRoutes = require("./user");
-const adminEditRoutes = require("./admin");
+const userEditRoutes = require('./user');
+const adminEditRoutes = require('./admin');
 
-router.use("/user", ensureUserIsAuth, userEditRoutes);
-router.use("/admin", ensureAdminIsAuth, adminEditRoutes);
+router.use('/user', ensureUserIsAuth, userEditRoutes);
+router.use('/admin', ensureAdminIsAuth, adminEditRoutes);
 
 module.exports = router;
