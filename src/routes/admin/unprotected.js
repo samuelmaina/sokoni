eae154d0fdf32403d5fa5935b62f975ee9d754df
samuelmayna <samuelmayna@gmail.com>
2022-03-30 04:33:35 +0300

    Removed the product and query validators since they are not being used in any unprotected routes

--- src/routes/admin/unprotected.js
+++ src/routes/admin/unprotected.js
@@ -1,11 +1,6 @@
 const express = require("express");
 
 const { admin } = require("../../controllers");
-
-const { product, productQuery } = require("../../utils/validators");
-const productValidtor = product.productValidator;
-const productQueryValidator = productQuery.productQueryValidator;
-
 const controller = admin;
 
 const router = express.Router();
