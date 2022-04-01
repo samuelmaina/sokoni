a9b899e74c0794a25f997fa23634a099bb1713c9
samuelmayna <samuelmayna@gmail.com>
2022-04-02 02:40:52 +0300

    removed the credentials from the file

--- src/utils/smsSender.js
+++ src/utils/smsSender.js
@@ -1,19 +1,15 @@
-const accountSid = "AC177d0310a30cdba2aafc543cffe1e0dc";
-const authToken = "27cde09697195386627af59b0d2f8e8a";
+const { SMS_ACCOUNTSID, SMS_AUTHTOKEN } = require("../config/env");
+const client = require("twilio")(SMS_ACCOUNTSID, SMS_AUTHTOKEN);
 
-const messagingServiceSid = "MGbbe062c2830f0386bbded73451a81dd5";
-const client = require("twilio")(accountSid, authToken);
+const sender = "+19377649987";
 
 module.exports = async (message, toNumber) => {
   try {
     const response = await client.messages.create({
       body: message,
-      messagingServiceSid,
+      from: sender,
       to: toNumber,
     });
-    if (response) {
-      console.log(response);
-    }
   } catch (error) {
     throw error;
   }
