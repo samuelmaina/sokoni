38f9459d2d528eb682a2f15bfd72c667aed12297
samuelmayna <samuelmayna@gmail.com>
2022-04-15 15:25:22 +0300

    implemented the signUp and the confirmEmail usecases in the authentication. Dependancies are injected at runtime

---
+++ src/useCases/auth/base.js
@@ -0,0 +1,56 @@
+const { EMAIL, BASE_URL } = require("../../config/env");
+
+exports.signUp = async (
+  body,
+  type,
+  validationErrors,
+  Model,
+  EmailToken,
+  emailSender
+) => {
+  const { email, name } = body;
+  const results = {};
+  if (validationErrors) {
+    results.error = validationErrors;
+    return results;
+  }
+  const existingEmail = await Model.findByEmail(email);
+  if (existingEmail) {
+    results.error = "Email already exists.Please try another one.";
+    return results;
+  }
+
+  const token = await EmailToken.createOneForEmail(email);
+
+  const emailToSend = {
+    from: EMAIL,
+    to: email,
+    subject: "Email Confirmation",
+
+    html: `<h3> Email Confirmation</h3><p> ${name} Thanks for joining SM Online Shop. The online shop you can trust.</p>
+    <br><p>Please click the link to confirm your email :<a href=${BASE_URL}/auth/${type}/confirm-email/${token}>
+    Confirm Email</a></p>
+    <p>Please note you only have one hour to confirm your email.</p>
+    <br> Thank you`,
+  };
+  await Model.createOne(body);
+  await emailSender(emailToSend);
+  results.success = true;
+  return results;
+};
+
+exports.confirmEmail = async function (token, EmailToken, Model) {
+  const res = {};
+  const tokenDetails = await EmailToken.findTokenDetailsByToken(token);
+  if (!tokenDetails) {
+    res.error =
+      "Too late for confirmation or the token is incorrect. Please try again.";
+    return res;
+  }
+  const doc = await Model.findByEmail(tokenDetails.email);
+  doc.markEmailAsConfirmed();
+  await tokenDetails.delete();
+  res.success = true;
+  res.info = "Email confirmation succcessful.";
+  return res;
+};
