e2f5afbc19ffb7629ad8d6c9ef8e5aab3c1f38d7
samuelmayna <samuelmayna@gmail.com>
2022-05-18 14:39:08 +0300

    Add mechanism to hold  redirect the user after auth together with the data they have currently added

--- src/middlewares/auth/ensureUserIsAuth.js
+++ src/middlewares/auth/ensureUserIsAuth.js
@@ -1,9 +1,12 @@
 const { Flash } = require("../../utils");
+const { addRedirectUrlAndCurrentBodyData } = require("./utils");
 
 module.exports = (req, res, next) => {
   try {
     const url = `/auth/user/log-in`;
+
     if (!(req.session && req.session.isUserLoggedIn)) {
+      addRedirectUrlAndCurrentBodyData(req);
       return new Flash(req, res)
         .appendInfo("Your are required to log in to continue")
         .redirect(url);
