2d3b13d8b32f47d77ab82cdfe007596b5fbbcbf6
samuelmayna <samuelmayna@gmail.com>
2022-05-18 14:38:39 +0300

    Add mechanism to hold  redirect the admin after auth together with the data they have currently added

--- src/middlewares/auth/ensureAdminIsAuth.js
+++ src/middlewares/auth/ensureAdminIsAuth.js
@@ -1,9 +1,11 @@
 const { Flash } = require("../../utils");
+const { addRedirectUrlAndCurrentBodyData } = require("./utils");
 
 module.exports = (req, res, next) => {
   try {
     const url = `/auth/admin/log-in`;
     if (!(req.session && req.session.isAdminLoggedIn)) {
+      addRedirectUrlAndCurrentBodyData(req);
       return new Flash(req, res)
         .appendInfo("Your are required to log in to continue")
         .redirect(url);
