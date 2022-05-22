22f5e92bc2440766b65927d741ac59ad1451e728
samuelmayna <samuelmayna@gmail.com>
2022-05-22 10:50:05 +0300

    Made the button gray, made currency to be displayed in Kshs

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,28 +1,128 @@
-let total = getValueById("total");
+const quantity = getValueByName("quantity");
+const originalStyle = { ...document.getElementById("balance").style };
+
+let total = Number(getValueById("total"));
 let balance = getValueById("balance");
 
 let sellingPrice = document.getElementById("sellingPrice").value;
-
-let count = 0;
-
-function getValueById(id) {
-  return document.getElementById(id).innerText;
-}
-
-const quantity = getValueByName("quantity");
+let isChanged = false;
 quantity.addEventListener("keyup", (e) => {
-  const latest = getValueByName("quantity");
-  const current = Number((latest.value * sellingPrice).toFixed(2));
-  writeById("total", current);
-  writeById("balance", balance - current);
+  let { remaining, currentTotal } = getRemainingBalance();
+
+  currentTotal = formatIntoCurrency(currentTotal);
+
+  writeById("total", currentTotal);
+
+  if (remaining < 0) {
+    renderOutOfBalance();
+    hideButton(true);
+    isChanged = true;
+  } else {
+    if (isChanged) {
+      resetToNormalDislay();
+    }
+    hideButton(false);
+    writeById("balance", formatIntoCurrency(remaining));
+    isChanged = false;
+  }
+
+  function getRemainingBalance() {
+    const latestQuantity = getValueByName("quantity").value;
+    const added = Number(latestQuantity * sellingPrice);
+    const currentTotal = total + added;
+    return {
+      currentTotal,
+      remaining: balance - currentTotal,
+    };
+  }
+
+  function hideButton(predicate) {
+    const button = document.getElementById("push-to-cart-btn");
+
+    if (predicate) {
+      button.disabled = predicate;
+      button.style.filter = " grayscale(0.3)";
+    } else {
+      button.disabled = predicate;
+      button.style.filter = " grayscale(0)";
+    }
+  }
 });
 
 function getValueByName(name) {
   return document.getElementsByName(name)[0];
 }
 
+function getElamentsByClassName(className) {
+  return document.getElementsByClassName(className);
+}
+
+function getValueById(id) {
+  return document.getElementById(id).innerText;
+}
+
 function writeById(id, value) {
   document.getElementById(id).innerText = value;
 }
 
-console.log(total, balance, quantity);
+function resetToNormalDislay() {
+  document.getElementById("hidable").style.display = "block";
+  document.getElementById("balance").style = originalStyle;
+}
+
+function renderOutOfBalance() {
+  const div = document.getElementById("balance");
+  document.getElementById("hidable").style.display = "none";
+  div.innerText = "Balance too low,please reduced the quantity.";
+  div.style.color = "rgb(252, 23, 23)";
+  div.style.border = "3px solid rgb(252, 23, 23)";
+  div.style.boxShadow = "2px 5px 20px rgba(245, 175, 175, 0.5)";
+  div.style.borderRadius = "12px";
+}
+
+function formatIntoCurrency(real) {
+  const numberized = Number(real);
+  if (!(numberized >= 0)) {
+    throw Error(`Value ${numberized} is not a postive number`);
+  }
+  let currencyToken = "Kshs ";
+  return currencyToken.concat(formatFloat(numberized));
+}
+function formatFloat(real) {
+  if (typeof real !== "number") {
+    throw new Error("Value must be a number.");
+  }
+  const point = ".";
+  const separator = ",";
+
+  const numAsString = Number(real).toFixed(2);
+
+  const splitNumbers = numAsString.split(point);
+
+  const floatingPart = splitNumbers[1];
+  const wholeNum = splitNumbers[0];
+
+  let result = formatInt(wholeNum, separator);
+  result += point;
+  result += floatingPart;
+  return result;
+}
+
+function formatInt(num, separator) {
+  const numAsString = num.toString();
+  const N = numAsString.length;
+
+  let result = [];
+  for (let i = N - 1, j = 0; i >= 0; i--, j++) {
+    if (j > 0 && j % 3 == 0) {
+      result.push(separator);
+    }
+    result.push(numAsString.charAt(i));
+  }
+  result = result.reverse();
+  let final = "";
+  for (const char of result) {
+    final += char;
+  }
+  return final;
+}
