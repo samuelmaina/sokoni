25b198693c725f37205b51366907abb5de01ccbf
samuelmayna <samuelmayna@gmail.com>
2022-05-22 10:53:17 +0300

    Moved the currency formatter to the utils file. File will contain the utility methods used by frontend js files

---
+++ public/js/utils.js
@@ -0,0 +1,46 @@
+export function formatIntoCurrency(real) {
+  const numberized = Number(real);
+  if (!(numberized >= 0)) {
+    throw Error(`Value ${numberized} is not a postive number`);
+  }
+  let currencyToken = "Kshs ";
+  return currencyToken.concat(formatFloat(numberized));
+}
+export function formatFloat(real) {
+  if (typeof real !== "number") {
+    throw new Error("Value must be a number.");
+  }
+  const point = ".";
+  const separator = ",";
+
+  const numAsString = Number(real).toFixed(2);
+
+  const splitNumbers = numAsString.split(point);
+
+  const floatingPart = splitNumbers[1];
+  const wholeNum = splitNumbers[0];
+
+  let result = formatInt(wholeNum, separator);
+  result += point;
+  result += floatingPart;
+  return result;
+}
+
+export function formatInt(num, separator) {
+  const numAsString = num.toString();
+  const N = numAsString.length;
+
+  let result = [];
+  for (let i = N - 1, j = 0; i >= 0; i--, j++) {
+    if (j > 0 && j % 3 == 0) {
+      result.push(separator);
+    }
+    result.push(numAsString.charAt(i));
+  }
+  result = result.reverse();
+  let final = "";
+  for (const char of result) {
+    final += char;
+  }
+  return final;
+}
