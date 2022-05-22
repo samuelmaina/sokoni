1acc132a3137e3fec1e6a716ca75f10f30244bed
samuelmayna <samuelmayna@gmail.com>
2022-05-22 11:14:33 +0300

    Resolved requiring from utils.js.

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,3 +1,5 @@
+import { formatIntoCurrency } from "/js/utils.js";
+
 const quantity = getValueByName("quantity");
 const originalStyle = { ...document.getElementById("balance").style };
 
@@ -53,10 +55,6 @@ function getValueByName(name) {
   return document.getElementsByName(name)[0];
 }
 
-function getElamentsByClassName(className) {
-  return document.getElementsByClassName(className);
-}
-
 function getValueById(id) {
   return document.getElementById(id).innerText;
 }
@@ -78,51 +76,4 @@ function renderOutOfBalance() {
   div.style.border = "3px solid rgb(252, 23, 23)";
   div.style.boxShadow = "2px 5px 20px rgba(245, 175, 175, 0.5)";
   div.style.borderRadius = "12px";
-}
-
-function formatIntoCurrency(real) {
-  const numberized = Number(real);
-  if (!(numberized >= 0)) {
-    throw Error(`Value ${numberized} is not a postive number`);
-  }
-  let currencyToken = "Kshs ";
-  return currencyToken.concat(formatFloat(numberized));
-}
-function formatFloat(real) {
-  if (typeof real !== "number") {
-    throw new Error("Value must be a number.");
-  }
-  const point = ".";
-  const separator = ",";
-
-  const numAsString = Number(real).toFixed(2);
-
-  const splitNumbers = numAsString.split(point);
-
-  const floatingPart = splitNumbers[1];
-  const wholeNum = splitNumbers[0];
-
-  let result = formatInt(wholeNum, separator);
-  result += point;
-  result += floatingPart;
-  return result;
-}
-
-function formatInt(num, separator) {
-  const numAsString = num.toString();
-  const N = numAsString.length;
-
-  let result = [];
-  for (let i = N - 1, j = 0; i >= 0; i--, j++) {
-    if (j > 0 && j % 3 == 0) {
-      result.push(separator);
-    }
-    result.push(numAsString.charAt(i));
-  }
-  result = result.reverse();
-  let final = "";
-  for (const char of result) {
-    final += char;
-  }
-  return final;
 }
